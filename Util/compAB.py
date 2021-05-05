f6446654f066cbeb47c7f7502013f67383a6d318
Zahra Ali <zahrafali95@gmail.com>
2021-05-04 21:46:42 -0400

    start from 1 instead of 0

--- Util/compAB.py
+++ Util/compAB.py
@@ -1,9 +1,11 @@
 def compAB(vertex, C_prev, graph):
+  
   # global graph
   AnB = []
   # no need to intersect after, check in c prev in here, check for element in cPrev
   for i in C_prev:
-    if graph[vertex][i] == 1 and i > vertex:
+    if graph[vertex-1][i-1] == 1 and i > vertex:
+      # print(vertex, C_prev, i)
       AnB.append(i)
   # This calculates the value of A n B n C[l-1] (formula)
   # can elemintate this step
